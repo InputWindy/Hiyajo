7b8336fed193dd25e87341d193dece4456418789
unknown <luchunyi01@cn.net.ntes>
2026-08-17 23:14:56 +0800

    Remove CreatePlugin entry + commit generated docs for AssemblyImporter/Engine/Toolkit plugins

--- CreatePlugin.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-# Launch the new-plugin UI. On Linux there is no console hiding
-# (Windows uses WScript + pythonw for that), so run create_plugin_ui.py directly.
-set -u
-exec "$(dirname "$0")/Tools/maho_python.sh" "$(dirname "$0")/Tools/create_plugin_ui.py" "$@"
