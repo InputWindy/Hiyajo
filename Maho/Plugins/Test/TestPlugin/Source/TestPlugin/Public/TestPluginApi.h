b374c203b09c9e7513260260f40b4a44da9e65a9
unknown <luchunyi01@cn.net.ntes>
2026-08-18 00:10:43 +0800

    Reorganize: Basic/ (fixed infra) + Plugins/Tool/ + Plugins/App/

--- Maho/Plugins/Test/TestPlugin/Source/TestPlugin/Public/TestPluginApi.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <Core/Export.h>
-
-#ifdef MAHO_TESTPLUGIN_MODULE_EXPORTS
-#	define MAHO_TESTPLUGIN_API MAHO_EXPORT
-#else
-#	define MAHO_TESTPLUGIN_API MAHO_IMPORT
-#endif
--- Maho/Plugins/Test/TestPlugin/TestPlugin.cmake
+++
@@ -1,2 +0,0 @@
-# TestPlugin plugin: CoreMinimal
-maho_set_plugin_output_dirs(${_MOD_TARGET} "${_MOD_PLUGIN_DIR}")
